83498e02b6ff7ac7dddb824ac03618499f9cf904
Frank Adelardi <fmadelardi@gmail.com>
2016-08-02 22:59:24 +0200

    Adding namespaces

--- index.js
+++ index.js
@@ -6,26 +6,32 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket) {
-  io.emit('refresh users', getUsers());
-  socket.broadcast.emit('chat message', 'a user has connected');
-  socket.on('disconnect', function() {
-    io.emit('chat message', 'a user has disconnected');
+app.get('/chat', function(req, res) {
+  chatroom = io.of('/chatroom');
+  chatroom.on('connection', function(socket) {
+    chatroom.emit('refresh users', getUsers());
+    socket.broadcast.emit('chat message', 'a user has connected');
+    socket.on('disconnect', function() {
+      chatroom.emit('chat message', 'a user has disconnected');
+    });
+
+    socket.on('chat message', function(m) {
+      chatroom.emit('chat message', m);
+    });
   });
 
-  socket.on('chat message', function(m) {
-    io.emit('chat message', m);
-  });
-})
+  function getUsers() {
+      var users = [];
+      for (var prop in chatroom.connected) {
+        users.push(prop);
+      }
+      return users;
+  }
+
+  res.sendFile(__dirname + "/chat.html");
+});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
 
-function getUsers() {
-    var users = [];
-    for (var prop in io.sockets.connected) {
-      users.push(prop);
-    }
-    return users;
-}
