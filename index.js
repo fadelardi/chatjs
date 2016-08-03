bd642061a5a269f3a396945b92f5ca8e6d7439c8
Frank Adelardi <fmadelardi@gmail.com>
2016-08-03 21:55:35 +0200

    Adding username feature

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var userList = {
+  users : [], 
+  add: function(userObj) {
+    this.users.push(userObj);
+  },
+  remove: function(name) {
+    var foundIndex = -1;
+    for (var i=0; i<this.users.length;i++) {
+      if (this.users[i].name == name) {
+        foundIndex = i;
+        break;
+      }
+      this.users.splice(foundIndex, 1);
+    }
+  },
+  get : function() { return this.users;  }
+};
+
+var username;
 
 var chatroom = io.of('/chatroom');
 chatroom.on('connection', function(socket) {
-chatroom.emit('refresh users', getUsers());
-socket.broadcast.emit('chat message', 'a user has connected');
-socket.on('disconnect', function() {
-  chatroom.emit('chat message', 'a user has disconnected');
+  userList.add({id: socket.id, name: username});
+  chatroom.emit('refresh users', userList.get());
+  socket.broadcast.emit('chat message', username + ' has connected');
+  socket.on('disconnect', function() {
+    userList.remove(username);
+    chatroom.emit('refresh users', userList.get());
+    chatroom.emit('chat message', username + ' has disconnected');
+  });
+
+  socket.on('chat message', function(m) {
+    chatroom.emit('chat message', m);
+  });
 });
 
-socket.on('chat message', function(m) {
-  chatroom.emit('chat message', m);
+app.post('/chat', function(req, res, next) {
+  username = req.body.username;
+  next();
+}, function(req, res) {
+  res.sendFile(__dirname + "/chat.html");
 });
-});
-
-function getUsers() {
-    var users = [];
-    for (var prop in chatroom.connected) {
-      users.push(prop);
-    }
-    return users;
-}
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
-});
-
-app.get('/chat', function(req, res) {
-  res.sendFile(__dirname + "/chat.html");
-});
+})
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
