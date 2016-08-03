b84d5fbbb43ef8397e8661907f65ef0af9d1d75f
Frank Adelardi <fmadelardi@gmail.com>
2016-08-03 20:30:34 +0200

    Connection listeners should be outside of routes (should be done once globally)

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var chatroom = io.of('/chatroom');
+chatroom.on('connection', function(socket) {
+chatroom.emit('refresh users', getUsers());
+socket.broadcast.emit('chat message', 'a user has connected');
+socket.on('disconnect', function() {
+  chatroom.emit('chat message', 'a user has disconnected');
+});
+
+socket.on('chat message', function(m) {
+  chatroom.emit('chat message', m);
+});
+});
+
+function getUsers() {
+    var users = [];
+    for (var prop in chatroom.connected) {
+      users.push(prop);
+    }
+    return users;
+}
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/chat', function(req, res) {
-  chatroom = io.of('/chatroom');
-  chatroom.on('connection', function(socket) {
-    chatroom.emit('refresh users', getUsers());
-    socket.broadcast.emit('chat message', 'a user has connected');
-    socket.on('disconnect', function() {
-      chatroom.emit('chat message', 'a user has disconnected');
-    });
-
-    socket.on('chat message', function(m) {
-      chatroom.emit('chat message', m);
-    });
-  });
-
-  function getUsers() {
-      var users = [];
-      for (var prop in chatroom.connected) {
-        users.push(prop);
-      }
-      return users;
-  }
-
   res.sendFile(__dirname + "/chat.html");
 });
 
