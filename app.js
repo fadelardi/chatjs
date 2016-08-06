4a600786adbed8bd29609657a4b431d6727f5379
Frank Adelardi <fmadelardi@gmail.com>
2016-08-06 16:27:30 +0200

    Adding logic to message handling

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
+//Declaring some base actions for the app
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use('/static', express.static(__dirname + '/static'));
 
+//Object to handle aggregation/removal of users
 var userList = {
   users : [],
   add: function(userObj) {
@@ -27,24 +29,28 @@ var userList = {
   get : function() { return this.users;  }
 };
 
+//Current username
 var username;
 
+// Setting up chatroom and related actions
 var chatroom = io.of('/chatroom');
 chatroom.on('connection', function(socket) {
   userList.add({id: socket.id, name: username});
   chatroom.emit('refresh users', userList.get(), username);
-  socket.broadcast.emit('chat message', username + ' has connected');
+  socket.broadcast.emit('chat message', { data: username, type: 'join' });
   socket.on('disconnect', function() {
     userList.remove(username);
     chatroom.emit('refresh users', userList.get(), username);
-    chatroom.emit('chat message', username + ' has disconnected');
+    chatroom.emit('chat message', { data: username , type: 'leave' });
 });
 
-socket.on('chat message', function(m) {
-    chatroom.emit('chat message', m);
+socket.on('chat message', function(messageObj) {
+    messageObj.username = username;
+    chatroom.emit('chat message', messageObj);
   });
 });
 
+//Routes
 app.post('/chat', function(req, res, next) {
   username = req.body.username;
   next();
@@ -56,6 +62,7 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+//Setting up server
 http.listen(3000, function() {
   console.log('Chat started on *:3000');
 });
--- static/chat-client.js
+++ static/chat-client.js
@@ -1,21 +1,41 @@
 var socket = io('/chatroom');
 
 document.addEventListener('DOMContentLoaded', function() {
+  //Get all elements in the chat that will be manipulated
   var usersList = document.getElementById('users');
   var messages = document.getElementById('messages');
   var messageForm = document.getElementById('messageForm');
   var messageBox = document.getElementById('messageBox');
+  //Action for when we type a message
   messageForm.addEventListener('submit', function(e) {
-    socket.emit('chat message', messageBox.value)
+    socket.emit('chat message', { data: messageBox.value, type: '' });
     messageBox.value = '';
     e.preventDefault();
     return false;
   });
 
-  socket.on('chat message', function(m) {
-    messages.innerHTML += '<li>' + m + '</li>';
+  //Message handling logic
+  socket.on('chat message', function(messageObj) {
+    var messageClass = '';
+    var messagePreffix = '';
+    var messageSuffix = '';
+
+    switch(messageObj.type) {
+      case 'join':
+        messageClass = 'class="join"';
+        messageSuffix = ' has joined.';
+        break;
+      case 'leave':
+        messageClass = 'class="leave"';
+        messageSuffix = ' has left.';
+        break;
+      default:
+        messagePreffix = '<span class="message-header">' + messageObj.username + ':</span> ';
+    }
+    messages.innerHTML += '<li ' + messageClass  +'>' + messagePreffix + messageObj.data + messageSuffix + '</li>';
   });
 
+  //Refreshing the user list
   socket.on('refresh users', function(users, current) {
     var userClass = '';
     usersList.innerHTML = '';
